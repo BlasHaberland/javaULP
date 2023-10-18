0f0e2fc8f93545a3cdab323c97ba1dac7f004f9a
Blas Haberland <haberlandblas@gmail.com>
2023-10-18 18:54:59 -0300

    completado ejercico 02 de la guia 5

--- unidad-5/ej02/demo/src/main/java/ejercicio02/Main.java
+++ unidad-5/ej02/demo/src/main/java/ejercicio02/Main.java
@@ -2,6 +2,15 @@ package ejercicio02;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int n = 100;
+        int[] enteros = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            enteros[i] = i;
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(enteros[n - 1 - i]);
+        }
     }
 }
