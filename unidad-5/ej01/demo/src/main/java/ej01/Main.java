b583a1bc564356b0cd3f7543608850ae8bbe9716
Blas Haberland <haberlandblas@gmail.com>
2023-10-17 21:40:04 -0300

    Agregado metodo cuentaCaracter del punto b

--- unidad-5/ej01/demo/src/main/java/ej01/Main.java
+++ unidad-5/ej01/demo/src/main/java/ej01/Main.java
@@ -2,12 +2,14 @@ package ej01;
 
 public class Main {
     public static void main(String[] args) {
-        Arreglo.sumarLista(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+        // Arreglo.sumarLista(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
 
-        int[][] matriz = new int[2][];
-        matriz[0] = new int[] { 1, 2 };
-        matriz[1] = new int[] { 3, 4, 5, 6 };
-        System.out.println(Arreglo.buscarMayor(matriz));
-        System.out.println(Arreglo.cuentaVocales("algo"));
+        // int[][] matriz = new int[2][];
+        // matriz[0] = new int[] { 1, 2 };
+        // matriz[1] = new int[] { 3, 4, 5, 6 };
+        // System.out.println(Arreglo.buscarMayor(matriz));
+        // System.out.println(Arreglo.cuentaVocales("algo"));
+
+        System.out.println(Arreglo.cuentaCaracter("PolemIcO", 'o'));
     }
 }
